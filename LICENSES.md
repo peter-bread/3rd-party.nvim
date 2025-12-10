e8550b69a8a2adc247d7ef0d8032e17d42b3cb24
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-12-10 03:11:34 +0000

    feat(diffview): adds diffview module

---
+++ LICENSES.md
@@ -0,0 +1,7 @@
+# 3rd-party.nvim - Module License Summary
+
+This repository is a collection of independent Lua modules.
+
+Each module retains the license of its original source, including any modifications.
+
+## [diffview](./lua/diffview/)
---
+++ lua/thirdparty/diffview/init.lua
@@ -0,0 +1,81 @@
+---@class thirdparty.diffview
+local M = {}
+
+--[[ ---------------------------------------------------------------------- ]]
+--
+--[[ ------------------- START OF PUBLIC API FUNCTIONS. ------------------- ]]
+--
+--[[ ---------------------------------------------------------------------- ]]
+
+---Cycle between diff3_mixed and diff4_mixed.
+function M.cycle_merge_conflict_layouts()
+	local api = vim.api
+	local lib = require("diffview.lib")
+	local lazy = require("diffview.lazy")
+	local utils = lazy.require("diffview.utils")
+
+	local Diff3Mixed = lazy.access("diffview.scene.layouts.diff_3_mixed", "Diff3Mixed")
+	local Diff4Mixed = lazy.access("diffview.scene.layouts.diff_4_mixed", "Diff4Mixed")
+
+  -- stylua: ignore
+  local DiffView        = lazy.access("diffview.scene.views.diff.diff_view", "DiffView")
+	local FileHistoryView = lazy.access("diffview.scene.views.file_history.file_history_view", "FileHistoryView")
+
+	local layout_cycle = {
+		Diff3Mixed.__get(),
+		Diff4Mixed.__get(),
+	}
+
+	local view = lib.get_current_view()
+	if not view then
+		return
+	end
+
+	local files, cur_file
+
+	if view:instanceof(FileHistoryView.__get()) then
+		---@cast view FileHistoryView
+		files = view.panel:list_files()
+		cur_file = view:cur_file()
+	elseif view:instanceof(DiffView.__get()) then
+		---@cast view DiffView
+		cur_file = view.cur_entry
+		if cur_file then
+			files = cur_file.kind == "conflicting" and view.files.conflicting
+				or utils.vec_join(view.panel.files.working, view.panel.files.staged)
+		end
+	else
+		return
+	end
+
+	-- Cycle layout for all file entries.
+	for _, entry in ipairs(files) do
+		local cur_layout = entry.layout.class
+		local idx = utils.vec_indexof(layout_cycle, cur_layout)
+		local next_layout = layout_cycle[(idx % #layout_cycle) + 1]
+		entry:convert_layout(next_layout)
+	end
+
+	-- Reopen current file to apply layout.
+	if cur_file then
+		local main = view.cur_layout:get_main_win()
+		local pos = api.nvim_win_get_cursor(main.id)
+		local was_focused = view.cur_layout:is_focused()
+
+		cur_file.layout.emitter:once("files_opened", function()
+			utils.set_cursor(main.id, unpack(pos))
+			if not was_focused then
+				view.cur_layout:sync_scroll()
+			end
+		end)
+
+		view:set_file(cur_file, false)
+		main = view.cur_layout:get_main_win()
+
+		if was_focused then
+			main:focus()
+		end
+	end
+end
+
+return M
