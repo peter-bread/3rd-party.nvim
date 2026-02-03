b07b86177467797439dbf74431c7759aafab0cff
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-02-03 15:48:09 +0000

    feat(mti): create user commands

---
+++ plugin/mason-tool-installer.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_user_command("MasonToolsClean", function()
+	require("thirdparty.mason-tool-installer").clean()
+end, { force = true })
+
+vim.api.nvim_create_user_command("MasonToolsEnsureInstalled", function()
+	require("thirdparty.mason-tool-installer").check_install()
+end, { force = true })
+
+vim.api.nvim_create_user_command("MasonToolsEnsureUpdated", function()
+	require("thirdparty.mason-tool-installer").check_install({ update = true })
+end, { force = true })
