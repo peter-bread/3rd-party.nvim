338fb3a272f2e98de09adad63838c3124f2cb612
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-02-09 21:34:18 +0000

    fix(mti): make sure public functions actually return values

--- lua/thirdparty/mason-tool-installer/init.lua
+++ lua/thirdparty/mason-tool-installer/init.lua
@@ -12,20 +12,21 @@ local M = {}
 ---Setup.
 ---@param opts thirdparty.mti.Config
 M.setup = function(opts)
-  require("thirdparty.mason-tool-installer.config").setup(opts)
+  return require("thirdparty.mason-tool-installer.config").setup(opts)
 end
 
 ---@param opts thirdparty.mti.check-install.Opts
 M.check_install = function(opts)
-  require("thirdparty.mason-tool-installer.installer").check_install(opts)
+  -- stylua: ignore
+  return require("thirdparty.mason-tool-installer.installer").check_install(opts)
 end
 
 M.clean = function()
-  require("thirdparty.mason-tool-installer.installer").clean()
+  return require("thirdparty.mason-tool-installer.installer").clean()
 end
 
 M.get_ensure_installed_names = function()
-  require("thirdparty.mason-tool-installer.installer").get_ensure_installed_names()
+  return require("thirdparty.mason-tool-installer.installer").get_ensure_installed_names()
 end
 
 return M
